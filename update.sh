2e5f1331c4bbd54d928d27ea00c132d3d71579db
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2021-06-17 12:26:47 +0800

    Add "sudo", "list", "autoremove" and disabled scripts to update OS

--- update.sh
+++ update.sh
@@ -1,9 +1,23 @@
 #!/usr/bin/env bash
 
-# OS Update Tool(WIP)
-# echo "Upgrade Ubuntu"
+## Use root without password prompt
+echo "Use root without password"
+sudo su
 
 #  Upgrade packages
 echo "Upgrade packages"
 apt update
+apt list --upgradable
 apt upgrade
+
+# Remove unused
+apt --purge autoremove -y
+
+## OS Update Tool(WIP) ref: do-release-upgrade -d
+# echo "Upgrade Ubuntu"
+# echo "Current version of OS and kernel"
+# lsb_release -a
+# echo "uname -mrs"
+# echo "Start to update"
+# apt install update-manager-core
+# do-release-upgrade -d
